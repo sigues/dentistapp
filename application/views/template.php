7da162ed84480542b5186c7911e2e071cc51d414
Salvador Villegas <salvador@sigues.org>
2012-10-08 18:08:19 -0500

    Cambios en template y empezando con odontograma

--- application/views/template.php
+++ application/views/template.php
@@ -46,6 +46,22 @@ function pideAjax(vurl){
 
 
 </script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35336608-1']);
+  _gaq.push(['_setDomainName', 'sigden.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </head>
 <body>
 <div id="wrapper">
@@ -131,6 +147,7 @@ function pideAjax(vurl){
                                                     <? if($this->session->userdata("idempleado")>0){?>
                                                     <li><?=anchor(base_url().'index.php/personal/logout','Cerrar Sesión')?></li>
                                                     <? } ?>
+                                                    <li><?=anchor(base_url().'index.php/personal','Panel de Control')?></li>
 							<li>
 								<h2>SIGDEN</h2>
 								<p>
@@ -138,7 +155,7 @@ function pideAjax(vurl){
                                                                     Este sistema le servirá para administrar todo lo relacionado con su consultorio.
                                                                 </p>
 							</li>
-							<li>
+							<!--<li>
 								<h2>Categories</h2>
 								<ul>
 									<li><a href="#">Aliquam libero</a></li>
@@ -149,7 +166,7 @@ function pideAjax(vurl){
 									<li><a href="#">Proin gravida orci porttitor</a></li>
 								</ul>
 							</li>
-							<!--<li>
+							<li>
 								<h2>Blogroll</h2>
 								<ul>
 									<li><a href="#">Aliquam libero</a></li>
